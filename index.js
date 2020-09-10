ccb9d062d6f775fc7fbc34d588b2042cdf8e28df
Catevika <dominique.bello@outlook.fr>
2020-09-10 19:56:46 +0200

    Refactoring code + layout, add contact-us background

--- index.js
+++ index.js
@@ -1,68 +1,88 @@
 // ICON + AUDIO MANAGEMENT - ENTER BUTTON CLICK
-const audioContainer = document.getElementById('audio-container');
+const mainContainer = document.getElementById('main-container');
+const enterContainer = document.getElementById('enter-container');
 const audio = document.getElementById('audio');
 const playBtn = document.getElementById('play');
 const playPause = document.getElementById('play-pause');
 
 // SOUND MANAGEMENT
-
-const sound = ['ring', 'ring', 'ring'];
+const sounds = ['ring', 'ring', 'ring'];
 let soundIndex = 1;
+let sound = sounds[soundIndex];
 
-function loadSound(sound)
-{
-  audio.src = `./sounds/${sound}.mp3`;
+function loadSound(sound) {
+	audio.src = `./sounds/${sound}.mp3`;
 }
 
-loadSound(sound[soundIndex]);
+loadSound(sound);
 
-function playSound()
-{
-  const pauseIsHidden = playPause.querySelector('img.icon-btn-pause').hasAttribute('hidden');
+function playSound() {
+	const pauseIsHidden = playPause
+		.querySelector('img.icon-btn-pause')
+		.hasAttribute('hidden');
 
-  if (pauseIsHidden) {
-    playPause.querySelector('img.icon-btn-pause').removeAttribute('hidden');
-    playPause.querySelector('img.icon-btn-arrow').setAttribute('hidden', "");
-  }
-  audioContainer.classList.add('play');
-  audio.play();
+	if (pauseIsHidden) {
+		playPause.querySelector('img.icon-btn-pause').removeAttribute('hidden');
+		playPause.querySelector('img.icon-btn-arrow').setAttribute('hidden', '');
+	}
+	mainContainer.classList.add('play');
+	audio.play();
 }
 
-function pauseSound()
-{
-  const arrowIsHidden = playPause.querySelector('img.icon-btn-arrow').hasAttribute('hidden');
+function pauseSound() {
+	const arrowIsHidden = playPause
+		.querySelector('img.icon-btn-arrow')
+		.hasAttribute('hidden');
 
-  if (arrowIsHidden) {
-    playPause.querySelector('img.icon-btn-arrow').removeAttribute('hidden');
-    playPause.querySelector('img.icon-btn-pause').setAttribute('hidden', "");
-  }
+	if (arrowIsHidden) {
+		playPause.querySelector('img.icon-btn-arrow').removeAttribute('hidden');
+		playPause.querySelector('img.icon-btn-pause').setAttribute('hidden', '');
+	}
 
-  audioContainer.classList.remove('play');
-  audio.pause();
+	mainContainer.classList.remove('play');
+	audio.pause();
 }
 
 // Next image
-function nextSound()
-{
-  soundIndex++;
-  if (soundIndex > sound.length - 1) {
-    soundIndex = 0;
-  }
-  loadSound(sound[soundIndex]);
-  playSound();
+function nextSound() {
+	soundIndex++;
+	if (soundIndex > sounds.length - 1) {
+		soundIndex = 0;
+	}
+	loadSound(sound);
+	playSound();
 }
 
-playBtn.addEventListener('click', () =>
-{
-  const isPlaying = audioContainer.classList.contains('play');
+enterContainer.addEventListener('click', () => {
+	const isPlaying = mainContainer.classList.contains('play');
 
-  if (isPlaying) {
-    playBtn.innerHTML = 'Enter';
-    pauseSound();
-  } else {
-    playBtn.innerHTML = 'Exit';
-    playSound();
-  }
+	if (isPlaying) {
+		playBtn.innerHTML = 'Enter';
+		pauseSound();
+	} else {
+		playBtn.innerHTML = 'Exit';
+		playSound();
+	}
 });
 
 audio.addEventListener('ended', nextSound);
+
+// SCROLL TO TOP MANAGEMENT
+const upArrowContainer = document.getElementById('about-us');
+const backToTopBtn = document.getElementById('up-arrow');
+
+// When the user scrolls down 50px from the top of the document, show the button
+window.addEventListener('scroll', () => {
+	if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+		upArrowContainer.querySelector('img.up-arrow').removeAttribute('hidden');
+	} else {
+		upArrowContainer.querySelector('img.up-arrow').setAttribute('hidden', '');
+	}
+});
+
+backToTopBtn.addEventListener('click', () => {
+	// When the user clicks on the button, scroll to the top of the document
+	document.body.scrollTop = 0; // For Safari
+	document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+	upArrowContainer.querySelector('img.up-arrow').setAttribute('hidden', '');
+});
